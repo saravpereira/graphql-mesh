7515984681d04d5e0427963e0a6d9393ddddabbc
Sara Pereira <sara.pereira@uniphore.com>
2023-06-07 19:39:20 -0700

    post call

--- client/graphql-mesh-client/src/App.js
+++ client/graphql-mesh-client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 import { Button } from "@mui/material";
 import axios from "axios";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { AntSwitch } from "./styles";
 
 const GET_QUERY = gql`
   {
@@ -28,6 +31,28 @@ const GET_QUERY = gql`
   }
 `;
 
+const POST_QUERY = gql`
+  mutation {
+    search3(
+      advSearchRequestInput: {
+        entity: "media-clip"
+        criteria: [{ name: "conversation", operator: "in", value: ${process.env.REACT_APP_POST_API_URL} }]
+      }
+    ) {
+      status
+      startTime
+      title
+      version
+      endTime
+      insertTime
+      id
+      userID {
+        fullname
+      }
+    }
+  }
+`;
+
 const options = {
   withCredentials: true,
   headers: {
@@ -42,23 +67,70 @@ function App() {
   const [dataset, setDataset] = useState({});
   const [restData, setRestData] = useState({});
   const [showGraphQL, setShowGraphQL] = useState(true);
+  const [showGetCall, setShowGetCall] = useState(true);
   const { loading, error, data } = useQuery(GET_QUERY);
+  const [
+    mutateFunction,
+    { loading: postLoading, error: postError, data: postData },
+  ] = useMutation(POST_QUERY);
 
   useEffect(() => {
-    console.log("DATA ****", data, error, loading);
-    setDataset(data);
-  }, [data, error, loading]);
+    if (!showGetCall) {
+      async function fetchGraphQL() {
+        const response = await mutateFunction({
+          variables: {
+            advSearchRequestInput: {
+              entity: "media-clip",
+              criteria: [
+                {
+                  name: "conversation",
+                  operator: "in",
+                  value: process.env.REACT_APP_POST_API_URL,
+                },
+              ],
+            },
+          },
+        });
+        setDataset(response);
+      }
+      fetchGraphQL();
+    }
+  }, [showGetCall]);
+
+  useEffect(() => {
+    if (showGetCall) setDataset(data);
+  }, [data, showGetCall]);
+
+  console.log("DATA ****", postData);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}`,
-        options
-      );
-      setRestData(response);
+      if (!showGetCall) {
+        const response = await axios.post(
+          process.env.REACT_APP_POST_API_URL,
+          {
+            entity: "media-clip",
+            criteria: [
+              {
+                name: "conversation",
+                operator: "in",
+                value: process.env.REACT_APP_TEST_ID_POST_CALL,
+              },
+            ],
+          },
+          options
+        );
+        setRestData(response);
+      } else {
+        const response = await axios.get(
+          process.env.REACT_APP_API_URL,
+          options
+        );
+        setRestData(response);
+      }
     }
     fetchData();
-  }, []);
+  }, [showGetCall]);
 
   return (
     <div
@@ -72,6 +144,16 @@ function App() {
         alignItems: "center",
       }}
     >
+      <Stack direction="row" spacing={1} alignItems="center">
+        <Typography>POST Call</Typography>
+        <AntSwitch
+          defaultChecked
+          inputProps={{ "aria-label": "ant design" }}
+          onChange={(e) => setShowGetCall(e.target.checked)}
+          checked={showGetCall}
+        />
+        <Typography>GET Call</Typography>
+      </Stack>
       <div
         style={{
           whiteSpace: "pre",
